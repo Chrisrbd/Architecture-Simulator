93f7d3ad4caa119038c25e93a47a8c590d0e428a
chrisrbd <christophe.reybaud@efrei.net>
2023-05-02 16:48:53 -0400

    Filtrer les commentaires + ajout du dossier asm-examples

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Test")
+filename = "example1.asm"
+
+with open("asm-examples/"+filename, "r") as f:
+
+    for line in f:
+        # Remove comments (filter)
+        parts = line.split("!", 1)
+        filtered_line = parts[0].strip()
+        if filtered_line:
+            # Print the code without comments
+            print(filtered_line)
