6b97c1f49b2c4cfd994e8d169e8e426ebfb7237c
ArTranArt <arthur.tran@efrei.net>
2023-05-02 23:59:11 -0400

    Ajout des fonctions LDA, ADD et STR Possibilité de voir les étapes dans le terminal

--- test.py
+++ test.py
@@ -1,26 +1,90 @@
-t0, t1, t2, t3 = 500, 0, 0, 0
-filename = "input2.asm"
+def func_lda(x, y):
+    if y.isdigit():
+        # If y is a number
+        data_vars[x] = int(y)
+    elif y in data_vars:
+        # If y is a variable
+        data_vars[x] = data_vars[y]
+    else:
+        raise ValueError(f"Variable {y} not found in data_vars")
 
-with open("asm-examples/"+filename, "r") as f:
-    lines = f.readlines()
 
-for i in range(len(lines)):
-    line = lines[i]
+def func_add(x, y):
+    if y in data_vars:
+        # If y is a variable
+        y_value = data_vars[y]
+    else:
+        y_value = int(y)
+    data_vars[x] += y_value
+
+
+def func_str(x, y):
+    if y.isdigit():
+        # If y is a number
+        data_vars[x] = int(y)
+    elif x.startswith("T"):
+        # If y is a register
+        print("Error: Register stores are not allowed.")
+    else:
+        # If y is a variable
+        data_vars[x] = data_vars[y]
+
+
+filename = "example1.asm"
+data_vars = {"T0": 0, "T1": 0, "T2": 0, "T3": 0}
+code_lines = []
+
+with open("asm-examples/"+filename) as f:
+    for line in f:
+        # Remove comments (filter)
+        parts = line.split("!", 1)
+        filtered_line = parts[0].strip()
+        if filtered_line:
+            # Append the filtered line to the list of code lines
+            code_lines.append(filtered_line)
+
+for i in range(len(code_lines)):
+    line = code_lines[i]
     if "#DATA" in line:
-        for j in range(i + 1, len(lines)):
-            var_line = lines[j].split()
+        for j in range(i + 1, len(code_lines)):
+            var_line = code_lines[j].split()
             if len(var_line) >= 2:
                 var_name = var_line[0]
                 var_value = int(var_line[1])
-                locals()[var_name] = var_value
+                data_vars[var_name] = var_value
             else:
+                print("Initial data values :")
+                print(data_vars)
+                print("")
                 break
+    elif "#CODE" in line:
+        for j in range(i + 1, len(code_lines)):
+            instr_line = code_lines[j].split()
+            instr_name = instr_line[0]
 
+            if len(instr_line) >= 2:
+                if instr_name == "gerg":
+                    print("salut")
 
-def add(x, y):
-    x = x + y
-    return x
+            if len(instr_line) >= 3:
+                operand1 = instr_line[1]
+                operand2 = instr_line[2]
+                if instr_name == "LDA":
+                    func_lda(operand1, operand2)
+                    print("LDA : " + operand1 + " ← " + operand2)
+                    print(data_vars)
+                    print("")
+                if instr_name == "ADD":
+                    func_add(operand1, operand2)
+                    print("ADD : " + operand1 + " ← " + operand1 + " + " + operand2)
+                    print(data_vars)
+                    print("")
+                if instr_name == "STR":
+                    func_str(operand1, operand2)
+                    print("STR : " + operand1 + " ← " + operand1 + " + " + operand2)
+                    print(data_vars)
+                    print("")
 
-print(t0)
-print(laptop)
-print(add(t0, laptop))
+## Voici la syntaxe pour les dictionnaires
+## print(data_vars["A"])
+## print("A =" + str(data_vars["A"]))
